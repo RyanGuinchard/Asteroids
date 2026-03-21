04dc936d55de346d4d4c7e7abd4f1c8295ef279e
Ryan Guinchard <ryanguinchard9@hotmail.com>
2026-03-21 19:22:42 -0230

    created character class

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 import constants as c
+from player import Player
 from logger import log_state
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print("Screen width: " + str(c.SCREEN_WIDTH))
     print("Screen height: " + str(c.SCREEN_HEIGHT))
@@ -18,7 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player = Player(c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
